7f62029064ed77905c0261a26b2024e1c07ad70e
Nate Ebel <n8ebel@goobar.dev>
2022-07-10 23:47:45 -0600

    Add lab 4 sample code

--- game-counter/app/src/main/java/dev/goobar/gamecounter/MainActivity.kt
+++ game-counter/app/src/main/java/dev/goobar/gamecounter/MainActivity.kt
@@ -2,11 +2,48 @@ package dev.goobar.gamecounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import dev.goobar.gamecounter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+  private val targetPoints = 20
+  private var currentPoints = 0
+
+  private lateinit var binding: ActivityMainBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-    helloWorld()
+
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    binding.targetPoints.text = "$targetPoints"
+    binding.currentPoints.text = "$currentPoints"
+
+    binding.resetButton.setOnClickListener {
+      currentPoints = 0
+      updateScoreText()
+
+      binding.plusOneButton.visibility = View.VISIBLE
+      binding.resetButton.visibility = View.GONE
+    }
+
+    binding.plusOneButton.setOnClickListener {
+      currentPoints++
+      updateScoreText()
+
+      if (currentPoints == targetPoints) {
+        Toast.makeText(this@MainActivity, "You Won!", Toast.LENGTH_SHORT).show()
+
+        binding.plusOneButton.visibility = View.GONE
+        binding.resetButton.visibility = View.VISIBLE
+      }
+    }
+  }
+
+  private fun updateScoreText() {
+    binding.currentPoints.text = "$currentPoints"
   }
 }
