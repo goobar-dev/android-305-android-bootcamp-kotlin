f8b97d9ea16bec517b45863689bfda72bdad2d7a
Nate Ebel <n8ebel@goobar.dev>
2022-07-15 01:46:22 -0600

    Add lab 8 sample code

--- android-study-guide/app/src/main/java/dev/goobar/androidstudyguide/CreateNoteFragment.kt
+++ android-study-guide/app/src/main/java/dev/goobar/androidstudyguide/CreateNoteFragment.kt
@@ -3,13 +3,13 @@ package dev.goobar.androidstudyguide
 import android.content.Context
 import android.os.Bundle
 import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import androidx.navigation.fragment.findNavController
-import com.google.android.material.internal.TextWatcherAdapter
 import com.google.android.material.snackbar.BaseTransientBottomBar
 import com.google.android.material.snackbar.Snackbar
 import dev.goobar.androidstudyguide.databinding.FragmentCreateNoteBinding
@@ -25,10 +25,60 @@ class CreateNoteFragment : Fragment() {
     savedInstanceState: Bundle?
   ): View? {
     binding = FragmentCreateNoteBinding.inflate(inflater, container, false)
-
     binding.categorySpinner.adapter = CategorySpinnerAdapter(requireContext(), CATEGORIES)
 
+    binding.titleEditText.addTextChangedListener(object : TextWatcher {
+      override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+      override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+      override fun afterTextChanged(s: Editable) {
+        // respond to changes in the title
+        binding.titleInputContainer.error = null
+        // if both title and content are not empty, show the save button
+        if (areInputsEntered()) {
+          binding.saveButton.show()
+        } else {
+          binding.saveButton.hide()
+        }
+      }
+    })
+
+    binding.noteEditText.addTextChangedListener(object : TextWatcher {
+      override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+      override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+      override fun afterTextChanged(s: Editable) {
+        // respond to changes in the title
+        binding.noteInputContainer.error = null
+        // if both title and content are not empty, show the save button
+        if (areInputsEntered()) {
+          binding.saveButton.show()
+        } else {
+          binding.saveButton.hide()
+        }
+      }
+    })
+
+    binding.saveButton.setOnClickListener {
+      if (areInputsEntered()) {
+        val snackbar = Snackbar.make(requireView(), "Saved the note!", Snackbar.LENGTH_SHORT)
+        snackbar.addCallback(object : BaseTransientBottomBar.BaseCallback<Snackbar>() {
+          override fun onDismissed(transientBottomBar: Snackbar?, event: Int) {
+            super.onDismissed(transientBottomBar, event)
+            findNavController().popBackStack()
+          }
+        })
+        snackbar.show()
+        return@setOnClickListener
+      }
+    }
+
     return binding.root
+  }
+
+  private fun areInputsEntered(): Boolean {
+    return binding.titleEditText.text.isNullOrBlank().not()
+        && binding.noteEditText.text.isNullOrBlank().not()
   }
 }
 
