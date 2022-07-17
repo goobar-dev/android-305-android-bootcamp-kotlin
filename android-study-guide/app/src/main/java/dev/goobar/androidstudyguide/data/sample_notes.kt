3cc551a65483b438d4b99138bc9fc088600d13b5
Nate Ebel <n8ebel@goobar.dev>
2022-07-16 18:17:58 -0600

    Add list of sample notes and add List<Notes> to MyNotesViewModel.UiState

---
+++ android-study-guide/app/src/main/java/dev/goobar/androidstudyguide/data/sample_notes.kt
@@ -0,0 +1,23 @@
+package dev.goobar.androidstudyguide.data
+
+val SAMPLE_NOTES = listOf(
+  Note("RecyclerView Info", "UI", "RecyclerView is efficient at showing many items on the screen."),
+  Note("Sample Note 2", "General", "Some sample content"),
+  Note(
+    title = "Kotlin is neat",
+    category = "Kotlin",
+    content = """Kotlin has a lot of great language features that were not previously available to Android developers.
+Features such as non-null types, extension functions, data classes, and higher-order functions.
+       """.trimIndent()
+  ),
+  Note("Sample Note 4", "Tooling", "Some sample content"),
+  Note("Sample Note 5", "UI", "Some sample content"),
+  Note("Sample Note 6", "Tooling", "Some sample content"),
+  Note("Sample Note 7", "General", "Some sample content"),
+  Note("Sample Note 8", "Kotlin", "Some sample content"),
+  Note("Sample Note 9", "Kotlin", "Some sample content"),
+  Note("Sample Note 10", "Misc", "Some sample content"),
+  Note("Sample Note 11", "General", "Some sample content"),
+  Note("Sample Note 12", "General", "Some sample content"),
+  Note("Sample Note 13", "General", "Some sample content"),
+)
--- android-study-guide/app/src/main/java/dev/goobar/androidstudyguide/mynotes/MyNotesViewModel.kt
+++ android-study-guide/app/src/main/java/dev/goobar/androidstudyguide/mynotes/MyNotesViewModel.kt
@@ -5,9 +5,10 @@ import kotlinx.coroutines.flow.MutableStateFlow
 
 class MyNotesViewModel : ViewModel() {
 
-  val state: MutableStateFlow<UiState> = MutableStateFlow(UiState("My Notes Fragment"))
+  val state: MutableStateFlow<UiState> = MutableStateFlow(UiState("My Notes Fragment", emptyList()))
 
   data class UiState(
-    val title: String
+    val title: String,
+    val notes: List<String>
   )
 }
